b80e5c5356517b889668c0af127377ea033f250f
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-02-17 14:57:48 -0800

    Sort repos and use yarn build instead of start

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 yarn install
 yarn backend
 # export SOURCECRED_GITHUB_TOKEN=YOUR_GITHUB_TOKEN
-curl -s https://api.github.com/orgs/nf-core/repos | grep full_name | cut -d \" -f 4 | grep -vE 'configs|tools' | xargs node bin/sourcecred.js load
-yarn start
+curl -s https://api.github.com/orgs/nf-core/repos | grep full_name | cut -d \" -f 4 | grep -vE 'configs|tools' | sort | xargs node bin/sourcecred.js load
+yarn build
