0b7a51b28af2011c9eaae173be7926a22c8b723e
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-02-17 14:17:35 -0800

    Comment out github token

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 yarn install
 yarn backend
-export SOURCECRED_GITHUB_TOKEN=YOUR_GITHUB_TOKEN
+# export SOURCECRED_GITHUB_TOKEN=YOUR_GITHUB_TOKEN
 curl -s https://api.github.com/orgs/nf-core/repos | grep full_name | cut -d \" -f 4 | grep -vE 'configs|tools' | xargs node bin/sourcecred.js load
